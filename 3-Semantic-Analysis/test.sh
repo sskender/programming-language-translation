5392dd7be1ea050bfae1036c350e1d2275b20818
sskender <sskender@users.noreply.github.com>
2020-12-02 15:24:13 +0100

    test script - added rating

--- 3-Semantic-Analysis/test.sh
+++ 3-Semantic-Analysis/test.sh
@@ -2,8 +2,19 @@
 
 TEST_FOLDER="tests"
 
+passed=0
+
 for t in $TEST_FOLDER/*;
 do
     python3.8 SemantickiAnalizator.py < $t/Test.in > /tmp/Test.out
     diff -s $t/Test.out /tmp/Test.out
+
+    if [[ $(diff -s $t/Test.out /tmp/Test.out ) == *"identical"* ]];
+    then
+        passed=$(($passed + 1))
+    fi
 done
+
+total=$(ls -l $TEST_FOLDER/ | wc -l)
+echo -e "\nPassed: $passed \nTotal : $total"
+
